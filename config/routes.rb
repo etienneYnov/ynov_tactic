e0a16d084cb1fa6ede3761137a75b3beb3597c98
Rock3f <badier.tanguy@gmail.com>
2017-02-28 10:28:04 +0100

    Revert "update"
    This reverts commit 627fedc87546aefbc9301a1ba77975528b80b0db.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :positions
   resources :markers
   resources :tactics
   resources :sequences
--- db/migrate/20170228092518_create_positions.rb
+++
@@ -1,9 +0,0 @@
-class CreatePositions < ActiveRecord::Migration[5.0]
-  def change
-    create_table :positions do |t|
-      t.string,x1 :name
-
-      t.timestamps
-    end
-  end
-end
