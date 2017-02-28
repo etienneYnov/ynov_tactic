cd3c2c7e53d0b941b8bf3ca2c45b6a46617bdcaa
Rock3f <badier.tanguy@gmail.com>
2017-02-28 10:14:47 +0100

    Migration

--- db/migrate/20170227105012_create_sequences.rb
+++ db/migrate/20170227105012_create_sequences.rb
@@ -2,7 +2,6 @@ class CreateSequences < ActiveRecord::Migration[5.0]
   def change
     create_table :sequences do |t|
       t.references :Tactic
-      t.integer :id
       t.string :name
 
       t.timestamps
--- db/migrate/20170227105151_create_tactics.rb
+++ db/migrate/20170227105151_create_tactics.rb
@@ -1,9 +1,7 @@
 class CreateTactics < ActiveRecord::Migration[5.0]
   def change
     create_table :tactics do |t|
-      t.integer :id
       t.string :name
-
       t.timestamps
     end
   end
